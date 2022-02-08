64f6882955cecd6cbdaf9eb01835f6a177a52efa
yseij <r0790156@student.thomasmore.be>
2022-02-08 13:05:48 +0100

    Beginnen met werken aan het gedeelte Uitgelichte berichten

---
+++ components/home-page/featured-posts.js
@@ -0,0 +1,8 @@
+function FeaturedPosts() {
+    return <section className={classes.latest}>
+        <h2>Featured Posts</h2>
+        
+    </section>
+}
+
+export default FeaturedPosts;
---
+++ components/posts/post-item.js
@@ -0,0 +1,5 @@
+function PostItem(){
+
+}
+
+export default PostItem
