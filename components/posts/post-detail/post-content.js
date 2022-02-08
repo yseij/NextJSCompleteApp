ee753bf3400a324ef86aa94c80514365ac71bd16
yseij <r0790156@student.thomasmore.be>
2022-02-08 15:29:50 +0100

    Markdown weergeven als JSX

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -1,3 +1,5 @@
+import ReactMarkdown from "react-markdown";
+
 import PostHeader from "./post-header";
 import classes from "./post-content.module.css";
 const DUMMY_POSTS = {
@@ -14,7 +16,7 @@ function PostContent() {
   return (
     <article className={classes.content}>
       <PostHeader title={DUMMY_POSTS.title} image={imagePath} />
-      {DUMMY_POSTS.content}
+      <ReactMarkdown>{DUMMY_POSTS.content}</ReactMarkdown>
     </article>
   );
 }
