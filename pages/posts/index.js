8b9aa4b589c3f6d0eb2619b8dea0b419a5952e2e
yseij <r0790156@student.thomasmore.be>
2022-02-08 13:59:09 +0100

    De pagina Alle berichten toevoegen

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,38 @@
-function AllPostPage() {}
+import AllPosts from "../../components/posts/all-posts";
+
+const DUMMY_POSTS = [
+  {
+    slug: "gettting-started1",
+    title: "getting started",
+    image: "getting-started-nextjs.png",
+    excerpt: "nextjs is goed",
+    date: "2022-02-10",
+  },
+  {
+    slug: "gettting-started2",
+    title: "getting started",
+    image: "getting-started-nextjs.png",
+    excerpt: "nextjs is goed",
+    date: "2022-02-10",
+  },
+  {
+    slug: "gettting-started3",
+    title: "getting started",
+    image: "getting-started-nextjs.png",
+    excerpt: "nextjs is goed",
+    date: "2022-02-10",
+  },
+  {
+    slug: "gettting-started4",
+    title: "getting started",
+    image: "getting-started-nextjs.png",
+    excerpt: "nextjs is goed",
+    date: "2022-02-10",
+  },
+];
+
+function AllPostPage() {
+  return <AllPosts posts={DUMMY_POSTS} />;
+}
 
 export default AllPostPage;
