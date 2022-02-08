53b97c408b926fac1f3c8781419765c5cf934be7
yseij <r0790156@student.thomasmore.be>
2022-02-08 13:45:50 +0100

    Dummy berichtgegevens weergeven

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,44 @@
 import { Fragment } from "react";
+import FeaturedPosts from "../components/home-page/featured-posts";
 
 import Hero from "../components/home-page/hero";
+
+const DUMMY_POSTS = [
+  {
+    slug: "gettting-started1",
+    title: "getting started",
+    image: "getting-started-nextjs.png",
+    excerpt: "nextjs is goed",
+    date: "2022-02-10",
+  },
+  {
+    slug: "gettting-started2",
+    title: "getting started",
+    image: "getting-started-nextjs.png",
+    excerpt: "nextjs is goed",
+    date: "2022-02-10",
+  },
+  {
+    slug: "gettting-started3",
+    title: "getting started",
+    image: "getting-started-nextjs.png",
+    excerpt: "nextjs is goed",
+    date: "2022-02-10",
+  },
+  {
+    slug: "gettting-started4",
+    title: "getting started",
+    image: "getting-started-nextjs.png",
+    excerpt: "nextjs is goed",
+    date: "2022-02-10",
+  },
+];
 
 function HomePage() {
   return (
     <Fragment>
       <Hero />
+      <FeaturedPosts posts={DUMMY_POSTS} />
     </Fragment>
   );
 }
