881712a7c2836f87888b6227a0f6a80388b3e632
yseij <r0790156@student.thomasmore.be>
2022-02-09 18:32:34 +0100

    wachtwoord weg gehaald bij env file

--- next.config.js
+++ next.config.js
@@ -3,18 +3,18 @@ const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
 module.exports = (phase) => {
   if (phase === PHASE_DEVELOPMENT_SERVER) {
     return {
-        env: {
-          mongodb_username: "nestjs",
-          mongodb_password: "1234",
-          mongodb_clustername: "cluster0",
-          mongobd_database: "my-site-dev",
-        },
-      };
+      env: {
+        mongodb_username: "nestjs",
+        mongodb_password: "",
+        mongodb_clustername: "cluster0",
+        mongobd_database: "my-site-dev",
+      },
+    };
   }
   return {
     env: {
       mongodb_username: "nestjs",
-      mongodb_password: "1234",
+      mongodb_password: "",
       mongodb_clustername: "cluster0",
       mongobd_database: "my-site",
     },
