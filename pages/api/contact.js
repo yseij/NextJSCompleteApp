86286a9909c31e64dd78f6e7d9211dc513617112
yseij <r0790156@student.thomasmore.be>
2022-02-09 12:34:39 +0100

    De Contact API-route toevoegen

---
+++ pages/api/contact.js
@@ -0,0 +1,31 @@
+function handler(req, res) {
+  if (req.method === "POST") {
+    const { email, name, message } = req.body;
+
+    if (
+      !email ||
+      !email.includes("@") ||
+      !name ||
+      !name.trim() === "" ||
+      !message ||
+      !message.trim() === ""
+    ) {
+      res.status(422).json({ message: "invalid input" });
+      return;
+    }
+
+    const newMessage = {
+      email,
+      name,
+      message,
+    };
+
+    console.log(newMessage);
+
+    res
+      .status(200)
+      .json({ message: "Succes data opgeslagen", message: newMessage });
+  }
+}
+
+export default handler;
